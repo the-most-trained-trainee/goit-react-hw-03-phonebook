a11645771adfa0b1d774ff8a7c24028a8a4d5e4c
the-most-trained-trainee <oleg.lavrenko@outlook.com>
2022-09-24 14:32:14 +0300

    Final Commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,13 +6,22 @@ import ContactList from './ContactList';
 
 class App extends React.Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
   };
+
+  componentDidMount() {
+    const localContacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(localContacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   contactSubmit = data => {
     this.setState(prevState => ({
